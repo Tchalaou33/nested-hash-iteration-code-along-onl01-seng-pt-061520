db9a4decede79a851f1eb435b52c74c0fced9a13
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-29 23:06:36 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,10 +14,12 @@ require 'pry'
      }
    }
   contacts. each do |person, contacts_details_hash|
-    
+    if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
     binding.pry
     # data.each do |attribute, value|
       
 # def remove_strawberry(contacts)
-
+end
+end
 end
