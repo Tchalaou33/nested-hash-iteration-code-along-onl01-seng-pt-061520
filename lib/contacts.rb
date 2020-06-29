f8cd6216bb028425c9f7749458dbbac67fd26ff8
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-29 23:22:14 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,15 +13,15 @@ require 'pry'
        favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
      }
    }
-  contacts.each do |person, contact_details_hash|
+  
+      
+ def remove_strawberry(contacts)
+   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
       data.delete_if {|ice_cream| ice_cream == "strawberry"}
-    # data.each do |attribute, value|
-      
- def remove_strawberry(contacts)
-   
+end
 end
 end
 end
